a28e20a02a9e41276b7d0d88aef7fdee279c7333
jannaeisenhut <121817318+jannaeisenhut@users.noreply.github.com>
2023-03-27 19:18:39 +0200

    Initial commit

---
+++ R/package_info.r
@@ -0,0 +1,9 @@
+ps.pkg.info = function() {
+  # Name of this package
+  package = "RTutorEnergyEfficience"
+  
+  # Name of problem sets in the package
+  ps = c("Energy-Efficience-low-income-households")
+  
+  list(package=package, ps = ps)
+}
